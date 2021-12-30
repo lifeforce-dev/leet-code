d84e1c0e340924c18c7a3abb68c64b55b65c2d1a
Joshua Sanders <joshua.sandersr@gmail.com>
2021-12-29 16:45:18 -0800

    optimize squares of sorted array

--- 977-squares-of-a-sorted-array/main.cpp
+++ 977-squares-of-a-sorted-array/main.cpp
@@ -4,13 +4,61 @@
 
 #include "common/Log.h"
 #include "common/Timer.h"
+#include "common/Random.h"
 
 namespace {
 	std::shared_ptr<spdlog::logger> s_logger;
 }
 
-std::vector<int> sortedSquares(std::vector<int>& nums)
+// Optimized, removes need for sort.
+std::vector<int> getSortedSquaresOptimized(std::vector<int>& nums)
 {
+	// r-sorted     sorted
+	//           |
+	// -4,-3,-2, 0, 1,3,6
+	// 16, 9, 4, 0, 2, 9, 36
+	// 
+	// move pointers inward from either side, dumping the appropriate squared
+	// value into the result array. Removes the need to call std::sort
+
+	Common::Timer timer;
+	timer.Start();
+
+	int count = nums.size();
+	std::vector<int> sortedSquares(count);
+	int leftIndex = 0;
+	int rightIndex = count - 1;
+	int valueToSquare = 0;
+
+	for (int i = count - 1; i >= 0; --i)
+	{
+
+		if (abs(nums[leftIndex]) < abs(nums[rightIndex]))
+		{
+			valueToSquare = nums[rightIndex];
+			rightIndex--;
+		}
+		else
+		{
+			valueToSquare = nums[leftIndex];
+			leftIndex++;
+		}
+
+		sortedSquares[i] = valueToSquare * valueToSquare;
+	}
+
+	timer.Stop();
+	SPDLOG_LOGGER_INFO(s_logger, "sortedSquaresOptimized() Time Elapsed: {}ms",
+		timer.GetTotalElapsedMs().count());
+	return sortedSquares;
+}
+
+// Naive version. Easy to read, probably fast enough.
+std::vector<int> getSortedSquares(std::vector<int>& nums)
+{
+	Common::Timer timer;
+	timer.Start();
+
 	std::vector<int> squares;
 	squares.reserve(nums.size());
 
@@ -19,6 +67,10 @@ std::vector<int> sortedSquares(std::vector<int>& nums)
 		squares.push_back(num * num);
 	}
 	std::sort(std::begin(squares), std::end(squares));
+
+	timer.Stop();
+	SPDLOG_LOGGER_INFO(s_logger, "sortedSquares() Time Elapsed: {}ms",
+		timer.GetTotalElapsedMs().count());
 	return squares;
 }
 
@@ -28,14 +80,21 @@ int main()
 	s_logger = Log::Logger("977. Squares Of A Sorted Array");
 
 	Common::Timer timer;
-
-	std::vector<int> nums = { -4,-1,0,3,10 };
-
 	timer.Start();
-	auto a = sortedSquares(nums);
+	std::vector<int> nums = Common::GetRandomNumberList(5000000, -10000000, 10000000);
+	std::sort(std::begin(nums), std::end(nums));
 	timer.Stop();
-	SPDLOG_LOGGER_INFO(s_logger, "Time Elapsed: {}",
-		timer.GetTotalElapsedNs().count());
+
+	SPDLOG_LOGGER_INFO(s_logger, "Building random numbers list Time Elapsed: {}ms",
+		timer.GetTotalElapsedMs().count());
+
+	std::vector<int> squaredNums = getSortedSquares(nums);
+	squaredNums = getSortedSquaresOptimized(nums);
+
+	// Sample metrics with these inputs
+	// Building random numbers list Time Elapsed : 438ms
+	// getSortedSquares() Time Elapsed           : 395ms
+	// getSortedSquaresOptimized() Time Elapsed  :  19ms
 
 	return 0;
 }
