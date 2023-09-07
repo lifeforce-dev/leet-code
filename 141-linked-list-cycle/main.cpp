ab32228e019e5cd5d1fe6470084115424d539fb2
Joshua R Sanders <joshua.sandersr@gmail.com>
2023-09-07 01:46:19 -0700

    Add first project generated from script XD
    - 141-linked-list-cycle

---
+++ 141-linked-list-cycle/main.cpp
@@ -0,0 +1,17 @@
+#define CATCH_CONFIG_MAIN
+
+#include "Catch2/catch.hpp"
+#include "common/Log.h"
+#include "common/Timer.h"
+
+#include <algorithm>
+#include <assert.h>
+#include <vector>
+
+SCENARIO("<brief problem description>")
+{
+	GIVEN("<the current condition>")
+	{
+		REQUIRE(true);
+	}
+}
